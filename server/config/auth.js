33c24a1b97f44daff056446e1f6005cac8ad9147
scott gago <scottgago@scotts-MacBook-Air.local>
2016-05-30 18:41:52 -0700

    fixed the IP address

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'github' : {
-    clientID: '8329b07f3d71abbc8222',
-    clientSecret: 'd1e57980a22872321d20cf34ba4ece7f58d51647',
-    callbackURL: "http://localhost:3000/login/github/return"
+    clientID: '58a7aeed22f6866e9366',
+    clientSecret: '116b354f3c5b782ffc46df15aec8f92a783c9852',
+    callbackURL: "http://192.81.135.170/login/github/return"
 	}
 }
